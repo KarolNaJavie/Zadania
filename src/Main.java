83b96a32e42960e4859b8b8921fcf3af9444a0d5
KarolNaJavie <dsztacheta@gmail.com>
2025-11-25 10:30:44 +0100

    init commit

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+void main() {
+    //Sworz metodym i je wywolaj
+    // Zadanie 1 – policz ile liczb jest podzielnych przez 3
+    // Zadanie 2 – policz ile liczb jest podzielnych przez 3 i 5 jednocześnie
+    // Zadanie 3 – zwróć liczby podzielne przez 3 lub przez 5
+    // Zadanie 4 – liczby podzielne przez 3 ALE nie przez 5 (lub odwrotnie)
+    // Zadanie 5 – najmniejsza liczba
+    // Zadanie 6 – największa liczba|
+
+
+
+
+
+
+
+
+    // Zadanie 1
+    //  - Stwórz interface Figura z metodami:
+    //        double obliczPole();
+    //        double obliczObwod();
+    //  - Stwórz 2 klasy implementujące (np. Kwadrat, Prostokat)
+    //  - Każda figura ma kolor i liczbę kątów
+    //  - W Main utwórz listę figur i wypisz:
+    //        kolor, liczba kątów, pole, obwód
+
+    // Zadanie 2
+    //  - Stwórz interfejs Movable (move())
+    //  - Stwórz interfejs Speakable (speak())
+    //  - Klasa Robot implementuje tylko Movable
+    //  - Klasa Human implementuje oba
+    //  - W Main wywołaj zachowania w pętli
+
+    // Zadanie 4 (ENUM)
+    //  - Program pyta użytkownika o kraj i kwotę brutto
+    //  - Enum Kraj zwraca kwotę netto
+    //  - Walidacja kraju ignoruje wielkość liter
+    //  - Zły kraj → komunikat, brak wyjątku dla użytkownika
+
+
+    //usun te ktore robiles i przypomnij mi zeby przerobic klasy abstrakcyjne
+    //jak sie bedziesz nudzil to obczaj co to sa regexy, to takie patterny np do znajdowania konkretnych fragmentow tekstu
+    //i obczaj te streingi co ci wyslalem
+
+
+}
